d7df7ac99b5033b460e633cff2924e58c2c5ca66
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-09-13 13:05:01 +0200

    Add alpha, provide some values with CompositionLocalProvider

--- app/src/main/java/hr/dtakac/prognoza/ui/theme/Theme.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/theme/Theme.kt
@@ -1,8 +1,6 @@
 package hr.dtakac.prognoza.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
@@ -26,6 +24,13 @@ data class PrognozaTypography(
     val contentSmall: TextStyle
 )
 
+@Immutable
+data class PrognozaContentAlpha(
+    val high: Float,
+    val medium: Float,
+    val disabled: Float
+)
+
 val LocalPrognozaColors = staticCompositionLocalOf {
     PrognozaColors(
         background = Color.Unspecified,
@@ -38,6 +43,14 @@ val LocalPrognozaTypography = staticCompositionLocalOf {
         contentProminent = TextStyle.Default,
         contentNormal = TextStyle.Default,
         contentSmall = TextStyle.Default
+    )
+}
+
+val LocalContentAlpha = staticCompositionLocalOf {
+    PrognozaContentAlpha(
+        high = 1f,
+        medium = 1f,
+        disabled = 1f
     )
 }
 
@@ -56,23 +69,31 @@ fun PrognozaTheme(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Black,
             fontSize = 24.sp,
-            letterSpacing = (-0.25).sp
+            letterSpacing = (-0.25).sp,
         ),
         contentNormal = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            letterSpacing = (-0.25).sp,
         ),
         contentSmall = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
+            fontSize = 16.sp,
         )
+    )
+
+    val alpha = PrognozaContentAlpha(
+        high = 0.87f,
+        medium = 0.6f,
+        disabled = 0.38f
     )
 
     CompositionLocalProvider(
         LocalPrognozaColors provides colors,
         LocalPrognozaTypography provides typography,
+        LocalContentAlpha provides alpha,
         content = content
     )
 }
@@ -85,4 +106,8 @@ object PrognozaTheme {
     val typography: PrognozaTypography
         @Composable
         get() = LocalPrognozaTypography.current
+
+    val alpha: PrognozaContentAlpha
+        @Composable
+        get() = LocalContentAlpha.current
 }
--- app/src/main/java/hr/dtakac/prognoza/ui/today/TodayScreen.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/today/TodayScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -19,6 +21,7 @@ import hr.dtakac.prognoza.presentation.TextResource
 import hr.dtakac.prognoza.presentation.today.TodayContent
 import hr.dtakac.prognoza.presentation.today.TodayHour
 import hr.dtakac.prognoza.presentation.today.TodayUiState
+import hr.dtakac.prognoza.ui.theme.LocalContentAlpha
 import hr.dtakac.prognoza.ui.theme.PrognozaTheme
 
 @Composable
@@ -30,109 +33,99 @@ fun TodayScreen(state: TodayUiState) {
 
 @Composable
 private fun Content(content: TodayContent) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(PrognozaTheme.colors.background)
-            .padding(horizontal = 32.dp)
+    CompositionLocalProvider(
+        LocalContentColor provides PrognozaTheme.colors.onBackground.copy(alpha = LocalContentAlpha.current.high),
+        LocalTextStyle provides PrognozaTheme.typography.contentNormal
     ) {
-        item {
-            Spacer(modifier = Modifier.height(64.dp))
-            Text(
-                text = content.placeName.asString(),
-                style = PrognozaTheme.typography.contentProminent,
-                color = PrognozaTheme.colors.onBackground
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = content.time.asString(),
-                style = PrognozaTheme.typography.contentNormal,
-                color = PrognozaTheme.colors.onBackground
-            )
-            ResponsiveText(
-                text = content.temperature.asString(),
-                style = PrognozaTheme.typography.contentProminent,
-                targetHeight = 250.sp,
-                color = PrognozaTheme.colors.onBackground
-            )
-            Row(modifier = Modifier.fillMaxWidth(),) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(PrognozaTheme.colors.background)
+                .padding(horizontal = 32.dp)
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(64.dp))
                 Text(
-                    modifier = Modifier.weight(1f),
-                    text = content.description.asString(),
+                    text = content.placeName.asString(),
                     style = PrognozaTheme.typography.contentProminent,
-                    color = PrognozaTheme.colors.onBackground
                 )
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = content.lowHighTemperature.asString(),
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = content.time.asString())
+                ResponsiveText(
+                    text = content.temperature.asString(),
                     style = PrognozaTheme.typography.contentProminent,
-                    color = PrognozaTheme.colors.onBackground,
-                    textAlign = TextAlign.End
+                    targetHeight = 250.sp,
                 )
+                Row(modifier = Modifier.fillMaxWidth(),) {
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = content.description.asString(),
+                        style = PrognozaTheme.typography.contentProminent,
+                    )
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = content.lowHighTemperature.asString(),
+                        style = PrognozaTheme.typography.contentProminent,
+                        textAlign = TextAlign.End
+                    )
+                }
+                Spacer(modifier = Modifier.height(42.dp))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = content.wind.asString(),
+                        style = PrognozaTheme.typography.contentSmall,
+                    )
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = content.precipitation.asString(),
+                        style = PrognozaTheme.typography.contentSmall,
+                        textAlign = TextAlign.End
+                    )
+                }
+                Spacer(modifier = Modifier.height(42.dp))
+                Text(
+                    text = stringResource(id = R.string.hourly),
+                    style = PrognozaTheme.typography.contentSmall,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Divider(color = PrognozaTheme.colors.onBackground)
+                Spacer(modifier = Modifier.height(16.dp))
             }
-            Spacer(modifier = Modifier.height(42.dp))
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = content.wind.asString(),
-                    style = PrognozaTheme.typography.contentSmall,
-                    color = PrognozaTheme.colors.onBackground
-                )
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = content.precipitation.asString(),
-                    style = PrognozaTheme.typography.contentSmall,
-                    color = PrognozaTheme.colors.onBackground,
-                    textAlign = TextAlign.End
-                )
+            items(content.hours) { hour ->
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        modifier = Modifier.width(52.dp),
+                        text = hour.time.asString(),
+                        style = PrognozaTheme.typography.contentSmall,
+                        textAlign = TextAlign.Start,
+                        maxLines = 1
+                    )
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = hour.description.asString(),
+                        style = PrognozaTheme.typography.contentSmall,
+                        textAlign = TextAlign.Start,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text(
+                        modifier = Modifier.width(88.dp),
+                        text = hour.precipitation.asString(),
+                        style = PrognozaTheme.typography.contentSmall,
+                        textAlign = TextAlign.End,
+                        maxLines = 1
+                    )
+                    Text(
+                        modifier = Modifier.width(52.dp),
+                        text = hour.temperature.asString(),
+                        style = PrognozaTheme.typography.contentSmall,
+                        textAlign = TextAlign.End,
+                        maxLines = 1
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
             }
-            Spacer(modifier = Modifier.height(42.dp))
-            Text(
-                text = stringResource(id = R.string.hourly),
-                style = PrognozaTheme.typography.contentSmall,
-                color = PrognozaTheme.colors.onBackground
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Divider(color = PrognozaTheme.colors.onBackground)
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-        items(content.hours) { hour ->
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Text(
-                    modifier = Modifier.width(52.dp),
-                    text = hour.time.asString(),
-                    style = PrognozaTheme.typography.contentSmall,
-                    color = PrognozaTheme.colors.onBackground,
-                    textAlign = TextAlign.Start,
-                    maxLines = 1
-                )
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = hour.description.asString(),
-                    style = PrognozaTheme.typography.contentSmall,
-                    color = PrognozaTheme.colors.onBackground,
-                    textAlign = TextAlign.Start,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    modifier = Modifier.width(88.dp),
-                    text = hour.precipitation.asString(),
-                    style = PrognozaTheme.typography.contentSmall,
-                    color = PrognozaTheme.colors.onBackground,
-                    textAlign = TextAlign.End,
-                    maxLines = 1
-                )
-                Text(
-                    modifier = Modifier.width(52.dp),
-                    text = hour.temperature.asString(),
-                    style = PrognozaTheme.typography.contentSmall,
-                    color = PrognozaTheme.colors.onBackground,
-                    textAlign = TextAlign.End,
-                    maxLines = 1
-                )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
