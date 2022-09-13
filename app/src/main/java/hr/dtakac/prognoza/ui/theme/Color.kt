89540088eac1f7ce9188924336c8b30b232ad5a6
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-09-13 15:18:56 +0200

    Start adding esri colors

--- app/src/main/java/hr/dtakac/prognoza/ui/theme/Color.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/theme/Color.kt
@@ -2,6 +2,19 @@ package hr.dtakac.prognoza.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+private val white = Color.White
+private val black = Color.Black
+
+// From https://www.esri.com/arcgis-blog/products/arcgis-pro/mapping/a-meaningful-temperature-palette/
+val extreme_cold_1 = Color(0xFFE4EFFE)
+val on_extreme_cold_1 = black
+
+val extreme_cold_2 = Color(0xFFDCE9FA)
+val on_extreme_cold_2 = black
+
+val extreme_cold_3 = Color(0xFFD3E2F7)
+val on_extreme_cold_3 = black
+
 val prognoza_theme_light_primary = Color(red = 221, green = 186, blue = 49, alpha = 255)
 val prognoza_theme_light_onPrimary = Color(red = 0, green = 0, blue = 0, alpha = 255)
 
