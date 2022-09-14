270b5a4082877878b8c6edf3a2cee37c9831bd85
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-09-14 17:15:35 +0200

    I think not

--- app/src/main/java/hr/dtakac/prognoza/ui/theme/Color.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/theme/Color.kt
@@ -5,9 +5,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
 import hr.dtakac.prognoza.entities.forecast.ShortForecastDescription
 
-private val mustard = Color(0xFFFFDD4A)
-private val yaleBlue = Color(0xFF084887)
-private val blueGray100 = Color(0xFFCFD8DC)
+private val oldGold = Color(0xFFDCBA31)
+private val stPatricksBlue = Color(0xFF123285)
+private val blueGray200 = Color(0xFFB0BEC5)
 private val gray50 = Color(0xFFFAFAFA)
 private val backgroundDark = Color(0xFF121212)
 
@@ -22,11 +22,11 @@ data class PrognozaColors(
             useDarkTheme: Boolean
         ): PrognozaColors {
             val lightColors = when (description) {
-                ShortForecastDescription.CLEAR -> PrognozaColors(mustard, Color.Black)
-                ShortForecastDescription.RAIN -> PrognozaColors(yaleBlue, Color.White)
+                ShortForecastDescription.CLEAR -> PrognozaColors(oldGold, Color.Black)
+                ShortForecastDescription.RAIN -> PrognozaColors(stPatricksBlue, Color.White)
                 ShortForecastDescription.SNOW -> PrognozaColors(gray50, Color.Black)
                 ShortForecastDescription.SLEET -> PrognozaColors(gray50, Color.Black)
-                ShortForecastDescription.CLOUDY -> PrognozaColors(blueGray100, Color.Black)
+                ShortForecastDescription.CLOUDY -> PrognozaColors(blueGray200, Color.Black)
             }
 
             return if (useDarkTheme) {
