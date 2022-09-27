9063a118e551a8b81da4190bd2a06c2d644ab33c
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-09-27 13:03:36 +0200

    Leave some todos

--- domain/src/main/java/hr/dtakac/prognoza/domain/usecase/GetSavedPlaces.kt
+++ domain/src/main/java/hr/dtakac/prognoza/domain/usecase/GetSavedPlaces.kt
@@ -5,6 +5,7 @@ import hr.dtakac.prognoza.domain.repository.PlaceRepositoryResult
 import hr.dtakac.prognoza.entities.Place
 
 class GetSavedPlaces(private val placeRepository: PlaceRepository) {
+    // todo: differentiate empty result and error
     suspend operator fun invoke(): List<Place> {
         val results = placeRepository.getSaved()
         return if (results is PlaceRepositoryResult.Success) results.data else listOf()
--- domain/src/main/java/hr/dtakac/prognoza/domain/usecase/SearchPlaces.kt
+++ domain/src/main/java/hr/dtakac/prognoza/domain/usecase/SearchPlaces.kt
@@ -5,6 +5,7 @@ import hr.dtakac.prognoza.domain.repository.PlaceRepositoryResult
 import hr.dtakac.prognoza.entities.Place
 
 class SearchPlaces(private val placeRepository: PlaceRepository) {
+    // todo: differentiate empty result and error
     suspend operator fun invoke(query: String): List<Place> {
         val result = placeRepository.search(query)
         return if (result is PlaceRepositoryResult.Success) result.data else listOf()
