c5ba71ea21d822b24ff9d191b930341da2f13a1a
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-09-28 11:35:37 +0200

    Apply theme to whole app, including settings

--- app/src/main/java/hr/dtakac/prognoza/ui/MainActivity.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/MainActivity.kt
@@ -3,29 +3,68 @@ package hr.dtakac.prognoza.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
+import hr.dtakac.prognoza.entities.forecast.ForecastDescription
+import hr.dtakac.prognoza.presentation.forecast.ForecastViewModel
 import hr.dtakac.prognoza.ui.forecast.ForecastScreen
 import hr.dtakac.prognoza.ui.settings.SettingsScreen
+import hr.dtakac.prognoza.ui.theme.PrognozaTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = "forecast") {
-                composable("forecast") {
-                    ForecastScreen(
-                        onSettingsClick = {
-                            navController.navigate("settings")
+            val forecastViewModel: ForecastViewModel = hiltViewModel()
+            val forecastState = remember { forecastViewModel.state }.value
+
+            PrognozaTheme(description = forecastState.forecast?.today?.shortDescription ?: ForecastDescription.Short.UNKNOWN) {
+                val backgroundColor = PrognozaTheme.backgroundColor
+                val elevatedBackgroundColor = PrognozaTheme.elevatedBackgroundColor
+                val onBackgroundColor = PrognozaTheme.onBackgroundColor
+
+                val systemUiController = rememberSystemUiController()
+                systemUiController.setSystemBarsColor(elevatedBackgroundColor)
+                systemUiController.setNavigationBarColor(elevatedBackgroundColor)
+
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "forecast") {
+                    composable("forecast") {
+                        val lifecycleOwner = LocalLifecycleOwner.current
+                        DisposableEffect(lifecycleOwner) {
+                            val observer = LifecycleEventObserver { _, event ->
+                                if (event == Lifecycle.Event.ON_RESUME) forecastViewModel.getState()
+                            }
+                            lifecycleOwner.lifecycle.addObserver(observer)
+                            onDispose { lifecycleOwner.lifecycle.removeObserver(observer) }
                         }
-                    )
-                }
-                composable("settings") {
-                    SettingsScreen()
+
+                        ForecastScreen(
+                            state = forecastState,
+                            backgroundColor = backgroundColor,
+                            elevatedBackgroundColor = elevatedBackgroundColor,
+                            onBackgroundColor = onBackgroundColor,
+                            onSettingsClick = {
+                                navController.navigate("settings")
+                            },
+                            onPlaceSelected = {
+                                forecastViewModel.getState()
+                            }
+                        )
+                    }
+                    composable("settings") {
+                        SettingsScreen()
+                    }
                 }
             }
         }
--- app/src/main/java/hr/dtakac/prognoza/ui/forecast/ForecastScreen.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/forecast/ForecastScreen.kt
@@ -8,17 +8,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import hr.dtakac.prognoza.entities.forecast.ForecastDescription
-import hr.dtakac.prognoza.presentation.forecast.ForecastViewModel
+import hr.dtakac.prognoza.presentation.forecast.ForecastState
 import hr.dtakac.prognoza.ui.theme.PrognozaTheme
 import hr.dtakac.prognoza.ui.places.PlacesScreen
 import kotlinx.coroutines.delay
@@ -27,151 +21,134 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ForecastScreen(
-    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
-    viewModel: ForecastViewModel = hiltViewModel(),
-    onSettingsClick: () -> Unit = {}
+    state: ForecastState,
+    backgroundColor: Color,
+    elevatedBackgroundColor: Color,
+    onBackgroundColor: Color,
+    onSettingsClick: () -> Unit = {},
+    onPlaceSelected: () -> Unit = {}
 ) {
-    // Refresh state every time screen is re-entered
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            if (event == Lifecycle.Event.ON_RESUME) viewModel.getState()
-        }
-        lifecycleOwner.lifecycle.addObserver(observer)
-        onDispose { lifecycleOwner.lifecycle.removeObserver(observer) }
-    }
-
-    val state by remember { viewModel.state }
     val forecast = state.forecast
     val error = state.error
 
-    PrognozaTheme(description = forecast?.today?.shortDescription ?: ForecastDescription.Short.UNKNOWN) {
-        val backgroundColor = PrognozaTheme.backgroundColor
-        val elevatedBackgroundColor = PrognozaTheme.elevatedBackgroundColor
-        val onBackgroundColor = PrognozaTheme.onBackgroundColor
-
-        val systemUiController = rememberSystemUiController()
-        systemUiController.setSystemBarsColor(elevatedBackgroundColor)
-        systemUiController.setNavigationBarColor(elevatedBackgroundColor)
-
-        val focusManager = LocalFocusManager.current
-        val drawerState = rememberDrawerState(DrawerValue.Closed)
-        LaunchedEffect(drawerState.isClosed) {
-            if (drawerState.isClosed) {
-                focusManager.clearFocus()
-            }
+    val focusManager = LocalFocusManager.current
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    LaunchedEffect(drawerState.isClosed) {
+        if (drawerState.isClosed) {
+            focusManager.clearFocus()
         }
-        val scope = rememberCoroutineScope()
+    }
+    val scope = rememberCoroutineScope()
 
-        ModalNavigationDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-                ModalDrawerSheet(
-                    drawerShape = RectangleShape,
-                    drawerContainerColor = elevatedBackgroundColor,
-                    drawerContentColor = onBackgroundColor
-                ) {
-                    PlacesScreen(
-                        onPlaceSelected = {
-                            scope.launch { drawerState.close() }
-                            viewModel.getState()
-                        },
-                        onSettingsClick = onSettingsClick,
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                drawerShape = RectangleShape,
+                drawerContainerColor = elevatedBackgroundColor,
+                drawerContentColor = onBackgroundColor
+            ) {
+                PlacesScreen(
+                    onPlaceSelected = {
+                        scope.launch { drawerState.close() }
+                        onPlaceSelected()
+                    },
+                    onSettingsClick = onSettingsClick,
+                    backgroundColor = elevatedBackgroundColor,
+                    contentColor = onBackgroundColor
+                )
+            }
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(backgroundColor)
+            ) {
+                var toolbarPlaceVisible by remember { mutableStateOf(false) }
+                var toolbarDateVisible by remember { mutableStateOf(false) }
+                var toolbarTemperatureVisible by remember { mutableStateOf(false) }
+
+                Box(contentAlignment = Alignment.BottomCenter) {
+                    ForecastToolbar(
+                        place = forecast?.place?.asString() ?: "",
+                        placeVisible = toolbarPlaceVisible,
+                        date = forecast?.today?.date?.asString() ?: "",
+                        dateVisible = toolbarDateVisible,
+                        temperature = forecast?.today?.temperature?.asString() ?: "",
+                        temperatureVisible = toolbarTemperatureVisible,
                         backgroundColor = elevatedBackgroundColor,
-                        contentColor = onBackgroundColor
-                    )
-                }
-            },
-            content = {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(backgroundColor)
-                ) {
-                    var toolbarPlaceVisible by remember { mutableStateOf(false) }
-                    var toolbarDateVisible by remember { mutableStateOf(false) }
-                    var toolbarTemperatureVisible by remember { mutableStateOf(false) }
-
-                    Box(contentAlignment = Alignment.BottomCenter) {
-                        ForecastToolbar(
-                            place = forecast?.place?.asString() ?: "",
-                            placeVisible = toolbarPlaceVisible,
-                            date = forecast?.today?.date?.asString() ?: "",
-                            dateVisible = toolbarDateVisible,
-                            temperature = forecast?.today?.temperature?.asString() ?: "",
-                            temperatureVisible = toolbarTemperatureVisible,
-                            backgroundColor = elevatedBackgroundColor,
-                            contentColor = onBackgroundColor,
-                            onMenuClick = {
-                                scope.launch {
-                                    drawerState.open()
-                                }
+                        contentColor = onBackgroundColor,
+                        onMenuClick = {
+                            scope.launch {
+                                drawerState.open()
                             }
+                        }
+                    )
+
+                    androidx.compose.animation.AnimatedVisibility(
+                        visible = state.isLoading,
+                        enter = fadeIn() + expandVertically(expandFrom = Alignment.Top),
+                        exit = fadeOut() + shrinkVertically(shrinkTowards = Alignment.Top)
+                    ) {
+                        LinearProgressIndicator(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(2.dp),
+                            color = onBackgroundColor,
+                            trackColor = elevatedBackgroundColor
+                        )
+                    }
+                }
+
+                if (forecast == null) {
+                    if (error != null) {
+                        ForecastError(
+                            text = error.asString(),
+                            backgroundColor = backgroundColor,
+                            contentColor = onBackgroundColor
+                        )
+                    }
+                } else {
+                    Box {
+                        ForecastContent(
+                            forecast = forecast,
+                            backgroundColor = backgroundColor,
+                            contentColor = onBackgroundColor,
+                            isPlaceVisible = { toolbarPlaceVisible = !it },
+                            isDateVisible = { toolbarDateVisible = !it },
+                            isTemperatureVisible = { toolbarTemperatureVisible = !it }
                         )
 
-                        androidx.compose.animation.AnimatedVisibility(
-                            visible = state.isLoading,
-                            enter = fadeIn() + expandVertically(expandFrom = Alignment.Top),
-                            exit = fadeOut() + shrinkVertically(shrinkTowards = Alignment.Top)
-                        ) {
-                            LinearProgressIndicator(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(2.dp),
-                                color = onBackgroundColor,
-                                trackColor = elevatedBackgroundColor
-                            )
-                        }
-                    }
-
-                    if (forecast == null) {
                         if (error != null) {
-                            ForecastError(
-                                text = error.asString(),
-                                backgroundColor = backgroundColor,
-                                contentColor = onBackgroundColor
-                            )
-                        }
-                    } else {
-                        Box {
-                            ForecastContent(
-                                forecast = forecast,
-                                backgroundColor = backgroundColor,
-                                contentColor = onBackgroundColor,
-                                isPlaceVisible = { toolbarPlaceVisible = !it },
-                                isDateVisible = { toolbarDateVisible = !it },
-                                isTemperatureVisible = { toolbarTemperatureVisible = !it }
-                            )
-
-                            if (error != null) {
-                                var showSnackBar by remember { mutableStateOf(false) }
-                                LaunchedEffect(forecast, error) {
-                                    scope.launch {
-                                        showSnackBar = true
-                                        delay(5000L)
-                                        showSnackBar = false
-                                    }
+                            var showSnackBar by remember { mutableStateOf(false) }
+                            LaunchedEffect(forecast, error) {
+                                scope.launch {
+                                    showSnackBar = true
+                                    delay(5000L)
+                                    showSnackBar = false
                                 }
+                            }
 
-                                // SnackBar needs to be prominent and stand out
-                                PrognozaTheme(
-                                    description = forecast.today.shortDescription,
-                                    useDarkTheme = !isSystemInDarkTheme()
-                                ) {
-                                    ForecastSnackBar(
-                                        modifier = Modifier
-                                            .align(Alignment.BottomCenter)
-                                            .padding(16.dp),
-                                        text = error.asString(),
-                                        visible = showSnackBar,
-                                        backgroundColor = elevatedBackgroundColor,
-                                        contentColor = onBackgroundColor
-                                    )
-                                }
+                            // SnackBar needs to be prominent and stand out
+                            PrognozaTheme(
+                                description = forecast.today.shortDescription,
+                                useDarkTheme = !isSystemInDarkTheme()
+                            ) {
+                                ForecastSnackBar(
+                                    modifier = Modifier
+                                        .align(Alignment.BottomCenter)
+                                        .padding(16.dp),
+                                    text = error.asString(),
+                                    visible = showSnackBar,
+                                    backgroundColor = elevatedBackgroundColor,
+                                    contentColor = onBackgroundColor
+                                )
                             }
                         }
                     }
                 }
             }
-        )
-    }
+        }
+    )
 }
