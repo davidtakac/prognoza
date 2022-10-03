a11547802b7973c52fcb734c113587bfec55f273
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-10-03 14:14:14 +0200

    Extract forecast content to separate file

--- app/src/main/java/hr/dtakac/prognoza/ui/forecast/ForecastContent.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/forecast/ForecastContent.kt
@@ -1,345 +1,121 @@
 package hr.dtakac.prognoza.ui.forecast
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hr.dtakac.prognoza.R
-import hr.dtakac.prognoza.entities.forecast.ForecastDescription
-import hr.dtakac.prognoza.presentation.TextResource
-import hr.dtakac.prognoza.presentation.forecast.TodayUi
-import hr.dtakac.prognoza.presentation.forecast.DayHourUi
-import hr.dtakac.prognoza.presentation.forecast.DayUi
-import hr.dtakac.prognoza.presentation.forecast.ForecastUi
+import hr.dtakac.prognoza.presentation.forecast.ForecastState
+import hr.dtakac.prognoza.ui.common.PrognozaSnackBar
+import hr.dtakac.prognoza.ui.common.PrognozaToolbar
 import hr.dtakac.prognoza.ui.theme.PrognozaTheme
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ForecastContent(
-    forecast: ForecastUi,
-    isPlaceVisible: (Boolean) -> Unit = {},
-    isDateVisible: (Boolean) -> Unit = {},
-    isTemperatureVisible: (Boolean) -> Unit = {}
+    state: ForecastState,
+    onMenuClick: () -> Unit = {}
 ) {
-    val horizontalPadding = 24.dp
-    val listState = rememberLazyListState()
-    LazyColumn(state = listState) {
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-        }
-        item(key = "place") {
-            Text(
-                text = forecast.place.asString(),
-                style = PrognozaTheme.typography.titleLarge,
-                modifier = Modifier.padding(horizontal = horizontalPadding)
-            )
-        }
-        item {
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-        item(key = "time") {
-            Text(
-                text = forecast.today.date.asString(),
-                style = PrognozaTheme.typography.subtitleLarge,
-                modifier = Modifier.padding(horizontal = horizontalPadding)
-            )
-        }
-        item(key = "temperature") {
-            AutoSizeText(
-                text = forecast.today.temperature.asString(),
-                style = PrognozaTheme.typography.headlineLarge,
-                maxFontSize = PrognozaTheme.typography.headlineLarge.fontSize,
-                maxLines = 1,
-                modifier = Modifier.padding(horizontal = horizontalPadding)
-            )
-        }
-        item {
-            DescriptionAndLowHighTemperature(
-                description = forecast.today.description.asString(),
-                lowHighTemperature = forecast.today.lowHighTemperature.asString(),
-                modifier = Modifier.padding(horizontal = horizontalPadding).fillMaxWidth()
-            )
-        }
-        item {
-            WindAndPrecipitation(
-                modifier = Modifier
-                    .padding(horizontal = horizontalPadding)
-                    .fillMaxWidth()
-                    .padding(top = 42.dp),
-                wind = forecast.today.wind.asString(),
-                precipitation = forecast.today.precipitation.asString()
-            )
-        }
-        item {
-            HourlyHeader(
-                modifier = Modifier
-                    .padding(horizontal = horizontalPadding)
-                    .fillMaxWidth()
-                    .padding(top = 42.dp, bottom = 16.dp)
-            )
-        }
-        items(forecast.today.hourly) { hour ->
-            HourItem(
-                hour = hour,
-                modifier = Modifier
-                    .padding(horizontal = horizontalPadding)
-                    .fillMaxWidth()
-                    .padding(bottom = 12.dp)
-            )
-        }
-        item {
-            ComingHeader(
-                modifier = Modifier
-                    .padding(horizontal = horizontalPadding)
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp, top = 30.dp)
-            )
-        }
-        items(forecast.coming) { day ->
-            ComingItem(
-                day = day,
-                modifier = Modifier
-                    .padding(horizontal = horizontalPadding)
-                    .fillMaxWidth()
-                    .padding(bottom = 20.dp)
-            )
-        }
-    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PrognozaTheme.colors.surface1)
+            .padding(WindowInsets.navigationBars.asPaddingValues())
+    ) {
+        var toolbarPlaceVisible by remember { mutableStateOf(false) }
+        var toolbarDateVisible by remember { mutableStateOf(false) }
+        var toolbarTemperatureVisible by remember { mutableStateOf(false) }
 
-    LaunchedEffect(listState) {
-        snapshotFlow { listState.layoutInfo }
-            .distinctUntilChanged()
-            .map { layoutInfo ->
-                Triple(
-                    layoutInfo.keyVisibilityPercent("place") != 0f,
-                    layoutInfo.keyVisibilityPercent("time") != 0f,
-                    layoutInfo.keyVisibilityPercent("temperature") > 50f
+        Box(contentAlignment = Alignment.BottomCenter) {
+            PrognozaToolbar(
+                title = { Text(state.forecast?.place?.asString() ?: "") },
+                subtitle = { Text(state.forecast?.today?.date?.asString() ?: "") },
+                end = { Text(state.forecast?.today?.temperature?.asString() ?: "") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onMenuClick,
+                        modifier = Modifier.size(48.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_menu),
+                            contentDescription = null
+                        )
+                    }
+                },
+                titleVisible = toolbarPlaceVisible,
+                subtitleVisible = toolbarDateVisible,
+                endVisible = toolbarTemperatureVisible
+            )
+
+            androidx.compose.animation.AnimatedVisibility(
+                visible = state.isLoading,
+                enter = fadeIn() + expandVertically(expandFrom = Alignment.Top),
+                exit = fadeOut() + shrinkVertically(shrinkTowards = Alignment.Top)
+            ) {
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(2.dp),
+                    color = PrognozaTheme.colors.onSurface,
+                    trackColor = Color.Transparent
                 )
             }
-            .distinctUntilChanged()
-            .collect { (isPlaceVisible, isDateVisible, isTemperatureVisible) ->
-                isPlaceVisible(isPlaceVisible)
-                isDateVisible(isDateVisible)
-                isTemperatureVisible(isTemperatureVisible)
+        }
+
+        if (state.forecast == null) {
+            if (state.error != null) {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp)
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = state.error.asString(),
+                        style = PrognozaTheme.typography.subtitleMedium,
+                        color = LocalContentColor.current.copy(alpha = PrognozaTheme.alpha.medium),
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
-    }
-}
-
-@Composable
-private fun DescriptionAndLowHighTemperature(
-    description: String,
-    lowHighTemperature: String,
-    modifier: Modifier = Modifier
-) {
-    Row(modifier = modifier) {
-        ProvideTextStyle(PrognozaTheme.typography.titleLarge) {
-            Text(
-                modifier = Modifier.weight(2f),
-                text = description
-            )
-            Text(
-                modifier = Modifier.weight(1f),
-                text = lowHighTemperature,
-                textAlign = TextAlign.End
-            )
-        }
-    }
-}
-
-@Composable
-private fun WindAndPrecipitation(
-    wind: String,
-    precipitation: String,
-    modifier: Modifier = Modifier
-) {
-    Row(modifier = modifier) {
-        ProvideTextStyle(PrognozaTheme.typography.body) {
-            Text(
-                modifier = Modifier.weight(1f),
-                text = wind,
-            )
-            Text(
-                modifier = Modifier.weight(1f),
-                text = precipitation,
-                textAlign = TextAlign.End
-            )
-        }
-    }
-}
-
-@Composable
-private fun HourlyHeader(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(
-            text = stringResource(id = R.string.hourly),
-            style = PrognozaTheme.typography.titleSmall,
-        )
-        Divider(
-            color = LocalContentColor.current,
-            modifier = Modifier.padding(top = 16.dp)
-        )
-    }
-}
-
-@Composable
-private fun ComingHeader(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(
-            text = stringResource(id = R.string.coming),
-            style = PrognozaTheme.typography.titleSmall,
-        )
-        Divider(
-            color = LocalContentColor.current,
-            modifier = Modifier.padding(top = 16.dp)
-        )
-    }
-}
-
-@Composable
-private fun HourItem(
-    hour: DayHourUi,
-    modifier: Modifier = Modifier
-) {
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        ProvideTextStyle(PrognozaTheme.typography.body) {
-            Text(
-                modifier = Modifier.width(52.dp),
-                text = hour.time.asString(),
-                textAlign = TextAlign.Start,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                modifier = Modifier.weight(1f),
-                text = hour.description.asString(),
-                textAlign = TextAlign.Start,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                modifier = Modifier.width(88.dp),
-                text = hour.precipitation.asString(),
-                textAlign = TextAlign.End,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                color = LocalContentColor.current.copy(alpha = PrognozaTheme.alpha.medium)
-            )
-            Text(
-                modifier = Modifier.width(52.dp),
-                text = hour.temperature.asString(),
-                textAlign = TextAlign.End,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Image(
-                painter = painterResource(id = hour.icon),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(start = 12.dp)
-                    .size(32.dp)
-            )
-        }
-    }
-}
-
-@Composable
-private fun ComingItem(
-    day: DayUi,
-    modifier: Modifier = Modifier
-) {
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        ProvideTextStyle(PrognozaTheme.typography.body) {
-            Text(
-                modifier = Modifier.weight(2f),
-                text = day.date.asString(),
-                textAlign = TextAlign.Start,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                modifier = Modifier.weight(1f),
-                text = day.precipitation.asString(),
-                textAlign = TextAlign.End,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                color = LocalContentColor.current.copy(alpha = PrognozaTheme.alpha.medium)
-            )
-            Text(
-                modifier = Modifier.weight(1f),
-                text = day.lowHighTemperature.asString(),
-                textAlign = TextAlign.End,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
-    }
-}
-
-@Preview
-@Composable
-private fun TodayScreenPreview() {
-    PrognozaTheme(description = ForecastDescription.Short.FAIR) {
-        ForecastContent(
-            forecast = ForecastUi(
-                place = TextResource.fromText("Helsinki"),
-                today = fakeTodayUi(),
-                coming = fakeComingUi()
-            )
-        )
-    }
-}
-
-private fun fakeTodayUi(): TodayUi = TodayUi(
-    date = TextResource.fromText("September 12"),
-    temperature = TextResource.fromText("1°"),
-    feelsLike = TextResource.fromText("Feels like 28°"),
-    description = TextResource.fromText("Clear sky, sleet soon"),
-    lowHighTemperature = TextResource.fromText("15°—7°"),
-    wind = TextResource.fromText("Wind: 15 km/h"),
-    precipitation = TextResource.fromText("Precipitation: 0 mm"),
-    shortDescription = ForecastDescription.Short.FAIR,
-    hourly = mutableListOf<DayHourUi>().apply {
-        for (i in 1..15) {
-            add(
-                DayHourUi(
-                    time = TextResource.fromText("14:00"),
-                    temperature = TextResource.fromText("23°"),
-                    precipitation = TextResource.fromText("1.99 mm"),
-                    description = TextResource.fromText("Clear and some more text"),
-                    icon = if (i % 2 == 0) R.drawable.heavysleetshowersandthunder_night else R.drawable.ic_question_mark
+        } else {
+            Box {
+                ForecastDataList(
+                    forecast = state.forecast,
+                    isPlaceVisible = { toolbarPlaceVisible = !it },
+                    isDateVisible = { toolbarDateVisible = !it },
+                    isTemperatureVisible = { toolbarTemperatureVisible = !it }
                 )
-            )
+
+                if (state.error != null) {
+                    val scope = rememberCoroutineScope()
+                    var showSnackBar by remember { mutableStateOf(false) }
+                    LaunchedEffect(state.forecast, state.error) {
+                        scope.launch {
+                            showSnackBar = true
+                            delay(5000L)
+                            showSnackBar = false
+                        }
+                    }
+                    PrognozaSnackBar(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(16.dp),
+                        text = state.error.asString(),
+                        visible = showSnackBar,
+                        backgroundColor = PrognozaTheme.colors.inverseSurface1,
+                        contentColor = PrognozaTheme.colors.onInverseSurface
+                    )
+                }
+            }
         }
     }
-)
-
-private fun fakeComingUi(): List<DayUi> = listOf(
-    DayUi(
-        date = TextResource.fromText("Thu, Sep 13"),
-        lowHighTemperature = TextResource.fromText("16—8"),
-        precipitation = TextResource.fromText(""),
-        icon = R.drawable.clearsky_day
-    ),
-    DayUi(
-        date = TextResource.fromText("Fri, Sep 14"),
-        lowHighTemperature = TextResource.fromText("18—8"),
-        precipitation = TextResource.fromText("0.7 mm"),
-        icon = R.drawable.rainshowers_day
-    ),
-    DayUi(
-        date = TextResource.fromText("Sat, Sep 15"),
-        lowHighTemperature = TextResource.fromText("21—5"),
-        precipitation = TextResource.fromText(""),
-        icon = R.drawable.cloudy
-    ),
-)
+}
--- app/src/main/java/hr/dtakac/prognoza/ui/forecast/ForecastScreen.kt
+++ app/src/main/java/hr/dtakac/prognoza/ui/forecast/ForecastScreen.kt
@@ -1,25 +1,12 @@
 package hr.dtakac.prognoza.ui.forecast
 
-import androidx.compose.animation.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import hr.dtakac.prognoza.R
 import hr.dtakac.prognoza.presentation.forecast.ForecastState
-import hr.dtakac.prognoza.ui.common.PrognozaSnackBar
 import hr.dtakac.prognoza.ui.theme.PrognozaTheme
 import hr.dtakac.prognoza.ui.places.PlacesScreen
-import hr.dtakac.prognoza.ui.common.PrognozaToolbar
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -57,99 +44,14 @@ fun ForecastScreen(
                 }
             },
             content = {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(PrognozaTheme.colors.surface1)
-                        .padding(WindowInsets.navigationBars.asPaddingValues())
-                ) {
-                    var toolbarPlaceVisible by remember { mutableStateOf(false) }
-                    var toolbarDateVisible by remember { mutableStateOf(false) }
-                    var toolbarTemperatureVisible by remember { mutableStateOf(false) }
-
-                    Box(contentAlignment = Alignment.BottomCenter) {
-                        PrognozaToolbar(
-                            title = { Text(state.forecast?.place?.asString() ?: "") },
-                            subtitle = { Text(state.forecast?.today?.date?.asString() ?: "") },
-                            end = { Text(state.forecast?.today?.temperature?.asString() ?: "") },
-                            navigationIcon = {
-                                IconButton(
-                                    onClick = { scope.launch { drawerState.open() } },
-                                    modifier = Modifier.size(48.dp)
-                                ) {
-                                    Icon(
-                                        painter = painterResource(id = R.drawable.ic_menu),
-                                        contentDescription = null
-                                    )
-                                }
-                            },
-                            titleVisible = toolbarPlaceVisible,
-                            subtitleVisible = toolbarDateVisible,
-                            endVisible = toolbarTemperatureVisible
-                        )
-
-                        androidx.compose.animation.AnimatedVisibility(
-                            visible = state.isLoading,
-                            enter = fadeIn() + expandVertically(expandFrom = Alignment.Top),
-                            exit = fadeOut() + shrinkVertically(shrinkTowards = Alignment.Top)
-                        ) {
-                            LinearProgressIndicator(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(2.dp),
-                                color = PrognozaTheme.colors.onSurface,
-                                trackColor = Color.Transparent
-                            )
+                ForecastContent(
+                    state = state,
+                    onMenuClick = {
+                        scope.launch {
+                            drawerState.open()
                         }
                     }
-
-                    if (state.forecast == null) {
-                        if (state.error != null) {
-                            Box(
-                                modifier = Modifier
-                                    .padding(horizontal = 24.dp)
-                                    .fillMaxSize(),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Text(
-                                    text = state.error.asString(),
-                                    style = PrognozaTheme.typography.subtitleMedium,
-                                    color = LocalContentColor.current.copy(alpha = PrognozaTheme.alpha.medium),
-                                    textAlign = TextAlign.Center
-                                )
-                            }
-                        }
-                    } else {
-                        Box {
-                            ForecastContent(
-                                forecast = state.forecast,
-                                isPlaceVisible = { toolbarPlaceVisible = !it },
-                                isDateVisible = { toolbarDateVisible = !it },
-                                isTemperatureVisible = { toolbarTemperatureVisible = !it }
-                            )
-
-                            if (state.error != null) {
-                                var showSnackBar by remember { mutableStateOf(false) }
-                                LaunchedEffect(state.forecast, state.error) {
-                                    scope.launch {
-                                        showSnackBar = true
-                                        delay(5000L)
-                                        showSnackBar = false
-                                    }
-                                }
-                                PrognozaSnackBar(
-                                    modifier = Modifier
-                                        .align(Alignment.BottomCenter)
-                                        .padding(16.dp),
-                                    text = state.error.asString(),
-                                    visible = showSnackBar,
-                                    backgroundColor = PrognozaTheme.colors.inverseSurface1,
-                                    contentColor = PrognozaTheme.colors.onInverseSurface
-                                )
-                            }
-                        }
-                    }
-                }
+                )
             }
         )
     }
