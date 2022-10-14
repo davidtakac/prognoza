6663fe4c294d49c732b00114441086dfddead000
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-10-14 10:48:31 +0200

    Remove some whitespace

--- data/src/main/java/hr/dtakac/prognoza/data/network/forecast/ForecastData.kt
+++ data/src/main/java/hr/dtakac/prognoza/data/network/forecast/ForecastData.kt
@@ -29,13 +29,10 @@ data class ForecastTimeStep(
 data class ForecastTimeStepData(
     @SerialName("instant")
     val instant: ForecastTimeInstant,
-
     @SerialName("next_1_hours")
     val next1Hours: ForecastTimePeriod? = null,
-
     @SerialName("next_6_hours")
     val next6Hours: ForecastTimePeriod? = null,
-
     @SerialName("next_12_hours")
     val next12Hours: ForecastTimePeriod? = null
 )
@@ -66,7 +63,6 @@ data class ForecastInstantData(
 data class ForecastTimePeriod(
     @SerialName("details")
     val data: ForecastTimePeriodData? = null,
-
     @SerialName("summary")
     val summary: ForecastTimePeriodSummary
 )
@@ -81,25 +77,18 @@ data class ForecastTimePeriodSummary(
 data class ForecastTimePeriodData(
     @SerialName("probability_of_thunder")
     val probabilityOfThunder: Double? = null,
-
     @SerialName("ultraviolet_index_clear_sky_max")
     val ultravioletIndexClearSkyMax: Double? = null,
-
     @SerialName("air_temperature_min")
     val airTemperatureMin: Double? = null,
-
     @SerialName("precipitation_amount_min")
     val precipitationAmountMin: Double? = null,
-
     @SerialName("precipitation_amount_max")
     val precipitationAmountMax: Double? = null,
-
     @SerialName("precipitation_amount")
     val precipitationAmount: Double? = null,
-
     @SerialName("air_temperature_max")
     val airTemperatureMax: Double? = null,
-
     @SerialName("probability_of_precipitation")
     val probabilityOfPrecipitation: Double? = null
 )
@@ -116,61 +105,42 @@ data class ForecastMeta(
 data class ForecastUnits(
     @SerialName("fog_area_fraction")
     val fogAreaFraction: String? = null,
-
     @SerialName("dew_point_temperature")
     val dewPointTemperature: String? = null,
-
     @SerialName("air_temperature_min")
     val airTemperatureMin: String? = null,
-
     @SerialName("relative_humidity")
     val relativeHumidity: String? = null,
-
     @SerialName("air_temperature_max")
     val airTemperatureMax: String? = null,
-
     @SerialName("cloud_area_fraction")
     val cloudAreaFraction: String? = null,
-
     @SerialName("air_pressure_at_sea_level")
     val airPressureAtSeaLevel: String? = null,
-
     @SerialName("cloud_area_fraction_high")
     val cloudAreaFractionHigh: String? = null,
-
     @SerialName("cloud_area_fraction_low")
     val cloudAreaFractionLow: String? = null,
-
     @SerialName("air_temperature")
     val airTemperature: String? = null,
-
     @SerialName("wind_speed")
     val windSpeed: String? = null,
-
     @SerialName("precipitation_amount_min")
     val precipitationAmountMin: String? = null,
-
     @SerialName("precipitation_amount_max")
     val precipitationAmountMax: String? = null,
-
     @SerialName("precipitation_amount")
     val precipitationAmount: String? = null,
-
     @SerialName("probability_of_precipitation")
     val probabilityOfPrecipitation: String? = null,
-
     @SerialName("wind_speed_of_gust")
     val windSpeedOfGust: String? = null,
-
     @SerialName("ultraviolet_index_clear_sky_max")
     val ultravioletIndexClearSkyMax: Int? = null,
-
     @SerialName("probability_of_thunder")
     val probabilityOfThunder: String? = null,
-
     @SerialName("wind_from_direction")
     val windFromDirection: String? = null,
-
     @SerialName("cloud_area_fraction_medium")
     val cloudAreaFractionMedium: String? = null
 )
