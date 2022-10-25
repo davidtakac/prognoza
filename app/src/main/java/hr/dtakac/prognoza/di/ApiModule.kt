c30e2ad9ad44c5f83b3dc34a0ba843e18ce8db5b
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-10-25 12:33:54 +0200

    Add API timeouts

--- app/src/main/java/hr/dtakac/prognoza/di/ApiModule.kt
+++ app/src/main/java/hr/dtakac/prognoza/di/ApiModule.kt
@@ -12,27 +12,46 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Retrofit
+import java.time.Duration
 import javax.inject.Singleton
 
-@Suppress("JSON_FORMAT_REDUNDANT")
 @OptIn(ExperimentalSerializationApi::class)
 @Module
 @InstallIn(SingletonComponent::class)
 class ApiModule {
+    // The json converter factory is a singleton, so the Json instance gets created only once
+    @Suppress("JSON_FORMAT_REDUNDANT")
     @Provides
     @Singleton
-    fun provideForecastService(): ForecastService {
+    fun provideJsonConverterFactory(): Converter.Factory = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+    }.asConverterFactory("application/json".toMediaType())
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
         val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        val okHttpClient = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+        val timeout = Duration.ofSeconds(10L)
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .callTimeout(timeout)
+            .readTimeout(timeout)
+            .writeTimeout(timeout)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideForecastService(
+        okHttpClient: OkHttpClient,
+        jsonConverterFactory: Converter.Factory
+    ): ForecastService {
         return Retrofit.Builder()
             .baseUrl("https://api.met.no/weatherapi/")
-            .addConverterFactory(
-                Json {
-                    ignoreUnknownKeys = true
-                    isLenient = true
-                }.asConverterFactory("application/json".toMediaType())
-            )
+            .addConverterFactory(jsonConverterFactory)
             .client(okHttpClient)
             .build()
             .create(ForecastService::class.java)
@@ -40,18 +59,13 @@ class ApiModule {
 
     @Provides
     @Singleton
-    fun providePlaceService(): PlaceService {
-        val loggingInterceptor =
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        val okHttpClient = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    fun providePlaceService(
+        okHttpClient: OkHttpClient,
+        jsonConverterFactory: Converter.Factory
+    ): PlaceService {
         return Retrofit.Builder()
             .baseUrl("https://nominatim.openstreetmap.org/")
-            .addConverterFactory(
-                Json {
-                    ignoreUnknownKeys = true
-                    isLenient = true
-                }.asConverterFactory("application/json".toMediaType())
-            )
+            .addConverterFactory(jsonConverterFactory)
             .client(okHttpClient)
             .build()
             .create(PlaceService::class.java)
