1b3cf7c60b453738170a9c14face55db20bdef63
davidtakac <35954251+davidtakac@users.noreply.github.com>
2022-11-04 10:11:18 +0100

    TAG in OsmPlaceSearcher

--- osmplacesearcher/src/main/java/hr/dtakac/prognoza/osmplacesearcher/OsmPlaceSearcher.kt
+++ osmplacesearcher/src/main/java/hr/dtakac/prognoza/osmplacesearcher/OsmPlaceSearcher.kt
@@ -4,14 +4,16 @@ import hr.dtakac.prognoza.domain.place.PlaceSearcher
 import hr.dtakac.prognoza.domain.place.PlaceSearcherResult
 import io.github.aakira.napier.Napier
 
+private const val TAG = "OsmPlaceSearcher"
+
 class OsmPlaceSearcher(
     private val osmPlaceService: OsmPlaceService
 ) : PlaceSearcher {
     override suspend fun search(query: String): PlaceSearcherResult {
         val entities = try {
-            osmPlaceService.search(query = query).map(PlaceResponse::toEntity)
+            osmPlaceService.search(query).map(PlaceResponse::toEntity)
         } catch (e: Exception) {
-            Napier.e(message = "OSM error", e)
+            Napier.e(TAG, e)
             null
         }
         return entities?.let {
